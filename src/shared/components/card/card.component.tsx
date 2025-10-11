097eb9da00d544f84ad7c3f6eaab13283f2694ae
Giovany Sievert <giovanysievert@gmail.com>
2025-10-11 11:58:29 -0300

    feature: add card component

---
+++ src/shared/components/card/card.component.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react'
+import { StyleSheet } from 'react-native'
+
+import { Box, BoxProps } from '../box'
+
+type CardProps = PropsWithChildren<BoxProps>
+
+export const Card: React.FC<CardProps> = ({ children, style, ...props }) => {
+  return (
+    <Box {...props} style={[styles.card, style]}>
+      {children}
+    </Box>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'transparent',
+    width: '100%',
+    borderColor: 'rgba(255,255,255,0.1)',
+    borderWidth: 1,
+    borderRadius: 12,
+    padding: 12
+  }
+})
