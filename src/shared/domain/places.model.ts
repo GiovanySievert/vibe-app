d4fcb0712f94ae24dfdfd2470d0bae990223bdcf
Giovany Sievert <giovanysievert@gmail.com>
2025-10-31 13:16:27 -0300

    feature: implment types

--- src/shared/domain/places.model.ts
+++ src/shared/domain/places.model.ts
@@ -7,3 +7,71 @@ export type PlacesModel = {
     lon: number
   }
 }
+
+export type Venue = {
+  id: number
+  brandId: number
+  name: string
+  priceRange: string
+  paymentMethods: string
+  socialInstagram: string
+  socialTiktok: string
+  contactPhone: string
+  about: string
+  createdAt: string
+  updatedAt: string
+}
+
+export type BrandMenu = {
+  id: number
+  brandId: number
+  name: string
+  description: string
+  priceCents: number
+  createdAt: string
+  updatedAt: string
+}
+
+export type Brand = {
+  id: number
+  name: string
+  taxId: string
+  type: string
+  avatar: string | null
+  createdAt: string
+  updatedAt: string
+  menus: BrandMenu[]
+}
+
+export type VenueLocation = {
+  id: number
+  venueId: number
+  addressLine: string
+  addressLine2: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  country: string
+  postalCode: string
+  lat: string
+  lng: string
+  createdAt: string
+  updatedAt: string
+}
+
+export type PlacesByIdResponse = {
+  id: number
+  brandId: number
+  name: string
+  priceRange: string
+  paymentMethods: string
+  socialInstagram: string
+  socialTiktok: string
+  contactPhone: string
+  about: string
+  createdAt: string
+  updatedAt: string
+  brand: Brand
+  location: VenueLocation
+}
--- src/shared/services/places.service.ts
+++ src/shared/services/places.service.ts
@@ -2,10 +2,10 @@ import { AxiosResponse } from 'axios'
 
 import { coreApi, placesApi } from '@src/services/api'
 
-import { PlacesRequestParamsDTO, PlacesResponseBodyDTO } from '../domain'
+import { PlacesByIdResponse, PlacesRequestParamsDTO, PlacesResponseBodyDTO } from '../domain'
 
 export const PlacesService = {
   fetchPlacesNearMe: (data: PlacesRequestParamsDTO): Promise<AxiosResponse<PlacesResponseBodyDTO>> =>
     placesApi.get(`/nearby?lat=${data.lat}&lon=${data.lon}&radius=${data.radius}`),
-  fetchPlaceById: (placeId: string): Promise<AxiosResponse<PlacesResponseBodyDTO>> => coreApi.get(`/venues/1`)
+  fetchPlaceById: (placeId: string): Promise<AxiosResponse<PlacesByIdResponse>> => coreApi.get(`/venues/1`)
 }
