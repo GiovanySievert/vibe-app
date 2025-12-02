efd6d0a728428ab3f8e32c1a6727be181732be80
Giovany Sievert <giovanysievert@gmail.com>
2025-12-02 11:27:21 -0300

    feature: implement user follow request list

---
+++ src/features/social/components/user-follow-request.component.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
+
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+
+import { AuthenticatedStackParamList } from '@src/app/navigation/types'
+import { FollowService } from '@src/features/users-profile/services'
+import { ListUserAllFollowRequestsResponse } from '@src/features/users-profile/types'
+import { authClient } from '@src/services/api/auth-client'
+import { Avatar, Box, Button, Divider, ThemedText } from '@src/shared/components'
+
+export const UserFollowRequests = () => {
+  const { data: userLoggedData } = authClient.useSession()
+  const navigation = useNavigation<NavigationProp<AuthenticatedStackParamList>>()
+  const queryClient = useQueryClient()
+
+  const fetchUser = async () => {
+    const response = await FollowService.listUserAllFollowRequests()
+    return response.data
+  }
+
+  const { data: userFollowRequestsData, isLoading } = useQuery<ListUserAllFollowRequestsResponse[], Error>({
+    queryKey: ['fetchUserById', userLoggedData?.user.id],
+    queryFn: fetchUser,
+    retry: false,
+    staleTime: 60 * 5
+  })
+
+  const acceptFollowRequest = async (requestFollowId: string) => {
+    try {
+      queryClient.setQueryData(['fetchUserById', userLoggedData?.user.id], (prevData: any[]) =>
+        prevData.filter((req) => req.id !== requestFollowId)
+      )
+
+      await FollowService.acceptFollowRequest(requestFollowId)
+    } catch {
+      queryClient.invalidateQueries({
+        queryKey: ['fetchUserById', userLoggedData?.user.id]
+      })
+    }
+  }
+
+  const rejectFollowRequest = async (requestFollowId: string) => {
+    try {
+      queryClient.setQueryData(['fetchUserById', userLoggedData?.user.id], (prevData: any[]) =>
+        prevData.filter((req) => req.id !== requestFollowId)
+      )
+
+      await FollowService.rejectFollowRequest(requestFollowId)
+    } catch {
+      queryClient.invalidateQueries({
+        queryKey: ['fetchUserById', userLoggedData?.user.id]
+      })
+    }
+  }
+
+  if (!userFollowRequestsData?.length || isLoading) {
+    return
+  }
+
+  return (
+    <Box mr={5} ml={5} gap={3}>
+      <ThemedText>Solicitações</ThemedText>
+      <Box gap={3}>
+        {userFollowRequestsData.map((userFollowRequest, index) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('Modals', {
+                  screen: 'UsersProfileScreen',
+                  params: { userId: userFollowRequest.requesterId }
+                })
+              }
+              key={userFollowRequest.id}
+            >
+              <Box flexDirection="column" justifyContent="space-between" gap={4} mb={4}>
+                <Box flexDirection="row" alignItems="center" gap={3}>
+                  <Avatar ring={0} uri={userFollowRequest.requesterAvatar} size="sm" />
+                  <ThemedText color="textPrimary" weight="semibold">
+                    {userFollowRequest.requesterUsername}
+                  </ThemedText>
+                </Box>
+                <Box flexDirection="row" gap={2}>
+                  <Button onPress={() => acceptFollowRequest(userFollowRequest.id)} flex={1}>
+                    <ThemedText variant="primary" weight="medium" size="lg">
+                      Aceitar
+                    </ThemedText>
+                  </Button>
+
+                  <Button onPress={() => rejectFollowRequest(userFollowRequest.id)} type="danger" flex={1}>
+                    <ThemedText variant="primary" weight="medium" size="lg">
+                      Rejeitar
+                    </ThemedText>
+                  </Button>
+                </Box>
+              </Box>
+              {index !== userFollowRequestsData.length - 1 && <Divider />}
+            </TouchableOpacity>
+          )
+        })}
+      </Box>
+    </Box>
+  )
+}
--- src/features/social/screens/social.screen.tsx
+++ src/features/social/screens/social.screen.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
 
 import { UserFavoritesPlacesCards } from '@src/features/user-favorites-places/components'
-import { Box, ThemedText } from '@src/shared/components'
+import { Box, Divider, ThemedText } from '@src/shared/components'
 import { Screen } from '@src/shared/components/screen'
+import { theme } from '@src/shared/constants/theme'
+
+import { UserFollowRequests } from '../components'
 
 export const SocialScreen = () => {
   return (
-    <Screen>
-      <Box pr={5} pl={5} mt={5} mb={5}>
-        <ThemedText variant="subtitle">Social</ThemedText>
-      </Box>
-      <UserFavoritesPlacesCards />
-    </Screen>
+    <ScrollView style={styles.scroll}>
+      <Screen>
+        <Box pr={5} pl={5} mt={5} mb={5}>
+          <ThemedText variant="subtitle">Social</ThemedText>
+        </Box>
+        <Box gap={5}>
+          <UserFavoritesPlacesCards />
+          <Box pr={5} pl={5}>
+            <Divider />
+          </Box>
+          <UserFollowRequests />
+        </Box>
+      </Screen>
+    </ScrollView>
   )
 }
+const styles = StyleSheet.create({
+  absoluteContainer: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 24,
+    padding: 24
+  },
+  relativeContainer: {
+    flex: 1
+  },
+  scroll: { flex: 1, backgroundColor: theme.colors.background }
+})
