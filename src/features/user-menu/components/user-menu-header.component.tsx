c3b2fd376f4d71ae49218ba33732085cb0a8a350
Giovany Sievert <giovanysievert@gmail.com>
2025-12-04 16:19:31 -0300

    feature: add shortcut to user profile page

--- src/features/user-menu/components/user-menu-header.component.tsx
+++ src/features/user-menu/components/user-menu-header.component.tsx
@@ -1,20 +1,38 @@
-import { Avatar, Box, Card, ThemedText } from '@src/shared/components'
+import { TouchableOpacity } from 'react-native'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
 
-export const UserMenuHeader = () => {
+import { AuthenticatedStackParamList } from '@src/app/navigation/types'
+import { Avatar, Box, Card, ThemedText } from '@src/shared/components'
+import { UserModel } from '@src/shared/domain/users.model'
+
+type userMenuHeaderProps = {
+  userData: UserModel
+}
+
+export const UserMenuHeader: React.FC<userMenuHeaderProps> = ({ userData }) => {
+  const navigation = useNavigation<NavigationProp<AuthenticatedStackParamList>>()
+
   return (
     <Card bg="background" gap={4}>
       <Box flexDirection="row" alignItems="center" justifyContent="space-between">
         <Box flexDirection="row" alignItems="center" gap={4}>
           <Avatar size="sm" />
-          <Box>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Modals', {
+                screen: 'UsersProfileScreen',
+                params: { userId: userData.id }
+              })
+            }
+          >
             <ThemedText variant="primary" size="lg" weight="semibold">
               Giovany Sievert
             </ThemedText>
 
             <ThemedText variant="secondary" size="sm">
-              giovanysievert@gmail.com
+              Ver perfil
             </ThemedText>
-          </Box>
+          </TouchableOpacity>
         </Box>
       </Box>
     </Card>
