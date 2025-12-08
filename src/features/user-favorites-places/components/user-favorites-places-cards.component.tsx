abdf79c86c6446a569f95211c4fdff7084e9c6a2
Giovany Sievert <giovanysievert@gmail.com>
2025-12-08 11:37:12 -0300

    refactor: change to flatlist

--- src/features/user-favorites-places/components/user-favorites-places-cards.component.tsx
+++ src/features/user-favorites-places/components/user-favorites-places-cards.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { FlatList, TouchableOpacity } from 'react-native'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 
 import { useAtom } from 'jotai'
@@ -8,39 +8,53 @@ import { AuthenticatedStackParamList } from '@src/app/navigation/types'
 import { Avatar, Box, Card, Divider, ThemedText } from '@src/shared/components'
 import { userFavoritesPlacesAtom } from '@src/shared/state'
 
+interface UserFavoritePlace {
+  id: string
+  venueId: string
+  name: string
+  avatar: string
+}
+
 export const UserFavoritesPlacesCards = () => {
   const navigation = useNavigation<NavigationProp<AuthenticatedStackParamList>>()
 
   const [userFavoritesPlaces] = useAtom(userFavoritesPlacesAtom)
 
+  const renderItem = ({ item, index }: { item: UserFavoritePlace; index: number }) => (
+    <Box>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Modals', {
+            screen: 'PlacesDetailsScreen',
+            params: { placeId: item.venueId }
+          })
+        }
+      >
+        <Box flexDirection="row" alignItems="center" gap={3}>
+          <Avatar ring={0} uri={item.avatar} size="sm" />
+          <ThemedText color="textPrimary" weight="medium">
+            {item.name}
+          </ThemedText>
+        </Box>
+      </TouchableOpacity>
+      {index !== userFavoritesPlaces.length - 1 && <Divider />}
+    </Box>
+  )
+
   if (!userFavoritesPlaces?.length) {
-    return <ThemedText>Sem favoritos</ThemedText>
+    return
   }
+
   return (
     <Box mr={5} ml={5} gap={3}>
       <ThemedText>Favoritos</ThemedText>
       <Card pr={5} pl={5} gap={3}>
-        {userFavoritesPlaces.map((userFavoritePlace, index) => {
-          return (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('Modals', {
-                  screen: 'PlacesDetailsScreen',
-                  params: { placeId: userFavoritePlace.venueId }
-                })
-              }
-              key={index}
-            >
-              <Box flexDirection="row" alignItems="center" gap={3} key={userFavoritePlace.id}>
-                <Avatar ring={0} uri={userFavoritePlace.avatar} size="sm" />
-                <ThemedText color="textPrimary" weight="medium">
-                  {userFavoritePlace.name}
-                </ThemedText>
-              </Box>
-              {index !== userFavoritesPlaces.length - 1 && <Divider />}
-            </TouchableOpacity>
-          )
-        })}
+        <FlatList
+          data={userFavoritesPlaces}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          scrollEnabled={false}
+        />
       </Card>
     </Box>
   )
