9a350eea26a70103c09ca7a4030e19a71e292f12
Giovany Sievert <giovanysievert@gmail.com>
2026-04-24 20:05:24 -0300

    feature: add linear gradient in screen

--- src/shared/components/screen/screen.component.tsx
+++ src/shared/components/screen/screen.component.tsx
@@ -2,15 +2,32 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import { LinearGradient } from 'expo-linear-gradient'
+
 import { theme } from '@src/shared/constants/theme'
 
 export type ScreenProps = {
   children: React.ReactNode
+  gradient?: boolean
 }
 
-export const Screen: React.FC<ScreenProps> = ({ children }) => {
+export const Screen: React.FC<ScreenProps> = ({ children, gradient }) => {
+  if (gradient) {
+    return (
+      <SafeAreaView style={styles.flex} edges={['right', 'left', 'top', 'bottom']}>
+        <LinearGradient
+          colors={['#252525', 'transparent']}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={styles.gradientOverlay}
+        />
+        {children}
+      </SafeAreaView>
+    )
+  }
+
   return (
-    <SafeAreaView style={[styles.flex]} edges={['right', 'left', 'top', 'bottom']}>
+    <SafeAreaView style={styles.flex} edges={['right', 'left', 'top', 'bottom']}>
       {children}
     </SafeAreaView>
   )
@@ -20,5 +37,23 @@ const styles = StyleSheet.create({
   flex: {
     flex: 1,
     backgroundColor: theme.colors.background
+  },
+  flexTransparent: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  gradientTop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 160
+  },
+  gradientOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   }
 })
